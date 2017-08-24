be4df26cd02ce0c0454145cce08157591cb2f429
kevintian <kevintian@pptv.com>
2017-08-24 17:04:53 +0800

    add:logger-config

--- src/main/java/com/example/web/ErrorController.java
+++ src/main/java/com/example/web/ErrorController.java
@@ -1,20 +1,27 @@
 package com.example.web;
 
 import com.example.common.JsonException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ErrorController {
+    private final static Logger logger = LoggerFactory.getLogger(ErrorController.class);
 
     @RequestMapping(value = "/error/default", method = RequestMethod.GET)
     public void defaultError() throws Exception {
-        throw new Exception("default error");
+        Exception error = new Exception("default error");
+        logger.error("", error);
+        throw error;
     }
 
     @RequestMapping(value = "/error/json", method = {RequestMethod.GET, RequestMethod.POST})
     public void jsonError() throws Exception {
-        throw new JsonException("json error");
+        Exception error = new JsonException("json error");
+        logger.error("", error);
+        throw error;
     }
 }
