72f4e89042102cb6d8fdabe0d6856e90731f2618
kevintian <kevintian@pptv.com>
2017-08-24 17:24:56 +0800

    add: scheduled tsk

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.*")
 @EnableMongoRepositories(basePackages = {"com.example.*"})
+@EnableScheduling
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/schedule/TimeReporter.java
@@ -0,0 +1,18 @@
+package com.example.schedule;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class TimeReporter {
+    private final static Logger logger = LoggerFactory.getLogger(TimeReporter.class);
+
+    @Scheduled(initialDelay = 5000, fixedRate = 5000)
+    public void reportTime() {
+        logger.info("!!! current time is: " + new Date());
+    }
+}
